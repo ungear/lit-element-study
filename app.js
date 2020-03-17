4ddd48d919c79b1628eee07f78053f3c631ba287
Mikhail Istomin <redeemer@inbox.ru>
2020-03-17 23:20:03 +0300

    basic element

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log(1)
+import { LitElement, html } from 'lit-element';
+
+// Extend the LitElement base class
+class MyElement extends LitElement {
+
+  /**
+   * Implement `render` to define a template for your element.
+   *
+   * You must provide an implementation of `render` for any element
+   * that uses LitElement as a base class.
+   */
+  render(){
+    /**
+     * `render` must return a lit-html `TemplateResult`.
+     *
+     * To create a `TemplateResult`, tag a JavaScript template literal
+     * with the `html` helper function:
+     */
+    return html`
+      <!-- template content -->
+      <p>A paragraph</p>
+    `;
+  }
+}
+// Register the new element with the browser.
+customElements.define('my-element', MyElement);
